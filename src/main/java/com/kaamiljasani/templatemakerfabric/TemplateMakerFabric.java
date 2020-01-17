a59718cacb23bbe6d408b27c654f6fc799fa69ff
Kaamil Jasani <kaamilj@yahoo.com>
2020-01-17 00:07:14 +0000

    Added methods for loading from urls

--- src/main/java/com/kaamiljasani/templatemakerfabric/TemplateMakerFabric.java
+++ src/main/java/com/kaamiljasani/templatemakerfabric/TemplateMakerFabric.java
@@ -1,7 +1,45 @@
 package com.kaamiljasani.templatemakerfabric;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 public class TemplateMakerFabric {
 
+    public static Document xmlFromUrl(String urlString) throws IOException, SAXException, ParserConfigurationException {
+        URL url = new URL(urlString);
+        return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url.openStream());
+    }
+
+    public static JsonElement jsonFromUrl(String url) throws IOException{
+        return JsonParser.parseString(getUrl(url));
+    }
+
+    public static String getUrl(String urlString) throws IOException{
+        URL url = new URL(urlString);
+        return readAllFromStream(url.openStream());
+    }
     
+    public static String readAllFromStream(InputStream stream) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while((line = reader.readLine()) != null){
+            result.append(line);
+            result.append("\n");
+        }
+        return result.toString();
+    }
 
 }
