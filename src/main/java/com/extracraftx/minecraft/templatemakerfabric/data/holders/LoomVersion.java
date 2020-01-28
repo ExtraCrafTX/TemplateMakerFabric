90a2055311f2493ccac310051d511716f1a09ac9
Kaamil Jasani <kaamilj@yahoo.com>
2020-01-28 22:45:50 +0000

    Added gradle version to LoomVersion

--- src/main/java/com/extracraftx/minecraft/templatemakerfabric/data/holders/LoomVersion.java
+++ src/main/java/com/extracraftx/minecraft/templatemakerfabric/data/holders/LoomVersion.java
@@ -3,10 +3,12 @@ package com.extracraftx.minecraft.templatemakerfabric.data.holders;
 public class LoomVersion{
     public final String name;
     public final int index;
+    public final int gradle;
 
     public LoomVersion(String name, int index){
         this.name = name;
         this.index = index;
+        this.gradle = index >= 3 ? 4 : 5;
     }
 
     @Override
@@ -19,5 +21,12 @@ public class LoomVersion{
      */
     public int getIndex() {
         return index;
+    }
+
+    /**
+     * @return the gradle major version
+     */
+    public int getGradle() {
+        return gradle;
     }
 }
